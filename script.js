2b83679b1237d6c62e21745c3596421cc6927e6c
Sudhanva Satish <71879797+suddu21@users.noreply.github.com>
2025-02-06 23:57:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,9 @@
 const messages = [
+    "Itna chubne laga hu sabko, kahi chura to nahi hu?",
+    "Tum jitna batate ho mere bare me, itna bura to nahi hu 😔",
     "Are you sure?",
-    "Really sure??",
-    "Are you positive?",
-    "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
-    "Just kidding, say yes please! ❤️"
+    "Ok fine, I will stop asking 😖",
+    "Hah bitch you thought, say yes or I will tell you horror stories every night"
 ];
 
 let messageIndex = 0;
@@ -19,7 +14,7 @@ function handleNoClick() {
     noButton.textContent = messages[messageIndex];
     messageIndex = (messageIndex + 1) % messages.length;
     const currentSize = parseFloat(window.getComputedStyle(yesButton).fontSize);
-    yesButton.style.fontSize = `${currentSize * 1.5}px`;
+    yesButton.style.fontSize = `${currentSize * 2.5}px`;
 }
 
 function handleYesClick() {
